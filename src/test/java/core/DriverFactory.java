1a2bcd73ad0b40e6cc59b6400b868157a7532280
Jundurian <g.fernandes@samsung.com>
2022-02-09 15:53:34 -0300

    GRID

--- src/test/java/core/DriverFactory.java
+++ src/test/java/core/DriverFactory.java
@@ -2,12 +2,18 @@ package core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
 
 //    private static WebDriver driver;
-      private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>(){
+      private static final ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>(){
           @Override
           protected synchronized WebDriver initialValue(){
               return initDriver();
@@ -22,7 +28,9 @@ public class DriverFactory {
 
     public static WebDriver initDriver(){
         WebDriver driver = null;
-            switch (Propriedades.browsers){
+
+        if (Propriedades.TIPO_EXECUCAO_VAR == Propriedades.TipoExecucao.LOCAL){
+            switch (Propriedades.BROWSERS_VAR){
                 case FIREFOX:
 //                    WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
@@ -31,8 +39,32 @@ public class DriverFactory {
 //                    WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     break;
+            }}
+
+        if (Propriedades.TIPO_EXECUCAO_VAR == Propriedades.TipoExecucao.GRID){
+
+            switch (Propriedades.BROWSERS_VAR){
+                case FIREFOX:
+//                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions options = new FirefoxOptions();
+                    try {
+                        driver = new RemoteWebDriver(new URL("http://localhost:4444/"),options);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case CHROME:
+//                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options2 = new ChromeOptions();
+                    try {
+                        driver = new RemoteWebDriver(new URL("http://localhost:4444/"),options2);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
             }
 
+        }
 
         return driver;
     }
--- src/test/java/core/Propriedades.java
+++ src/test/java/core/Propriedades.java
@@ -4,9 +4,9 @@ public class Propriedades {
 
     public static boolean FECHAR_BROWSER_DPS_CADA_TESTE = true;
 
-    public static Browsers browsers = Browsers.CHROME;
+    public static Browsers BROWSERS_VAR = Browsers.CHROME;
 
-    public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
+    public static TipoExecucao TIPO_EXECUCAO_VAR = TipoExecucao.GRID;
 //    public static String NOME_CONTA_ALTERADA = "Conta Alterada" + System.nanoTime();
 
     public enum Browsers{
